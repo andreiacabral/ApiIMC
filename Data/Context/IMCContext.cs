07863a740148902a577071e0c3e07c173fd6220a
Andreia Costa <andreiacabraldacosta@outlook.com>
2022-12-08 19:40:02 -0300

    Adicionado o String de conecção

--- Data/Context/IMCContext.cs
+++ Data/Context/IMCContext.cs
@@ -6,14 +6,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Data.Context
 {
     public class IMCContext : DbContext
     {
-        public IMCContext(DbContextOptions<IMCContext> options)
-    : base(options)
+        public IMCContext(DbContextOptions<IMCContext> options)  : base(options)
         {
+            
         }
 
         public DbSet<ConsultaIMC> Consultas { get; set; }
@@ -22,6 +23,12 @@ namespace Data.Context
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<ConsultaIMC>(new ConsultaIMCMap().Configure);
+
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=IMC;Trusted_Connection=true;");
         }
 
     }
